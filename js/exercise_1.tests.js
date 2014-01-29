3d9adc21b8fc168776c6049e3c813feb1bc277e0
Jon Borgonia <jon@gomagames.com>
2014-01-29 09:08:59 -1000

    added failing tests

---
+++ js/exercise_1.tests.js
@@ -0,0 +1,21 @@
+describe("A Random Number Generator", function() {
+  
+  it("should contain 1000 numbers", function() {
+    // generate 1000
+    var random_numbers = generateRandomNumbers(1000);
+
+    // should have 1000
+    expect(random_numbers.length).toBe(1000);
+  });
+
+  it("should generate different random numbers", function() {
+    // take 2 small samples of random numbers generated
+    var gen1 = generateRandomNumbers(50);
+    var gen2 = generateRandomNumbers(50);
+
+    // test that the first and last numbers are different
+    // expecting that there are 50 numbers in each array
+    expect(gen1[0]).not.toBe(gen2[0]);
+    expect(gen1[49]).not.toBe(gen2[49]);
+  });
+});
